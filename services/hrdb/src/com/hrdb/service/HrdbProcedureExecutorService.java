8828df1443b2c83b93dda0530d86b0000364b03b
Sushma Naga <sushma.maddirala@VCS2.com>
2016-09-07 10:50:33 +0000

    New files added - [EmployeeServiceImpl.java,HrdbQueryExecutorService.java,User.java,Department.java,hrdb_procedure.json,servicedef.xml,DepartmentService.java,DepartmentServiceImpl.java,hrdb.properties,DepartmentController.java,UserService.java,Employee.java,hrdb_published_dataModel.json,package-info.java,UserServiceImpl.java,hrdb-queries.hbm.xml,VacationController.java,hrdb.spring.xml,hrdb_query.json,VacationDao.java,HrdbProcedureExecutorService.java,UserController.java,EmployeeDao.java,hrdb.script,QueryExecutionController.java,hrdb-procedures.mappings.json,DepartmentDao.java,ProcedureExecutionController.java,db-connection-settings.json,VacationServiceImpl.java,EmployeeService.java,hrdb.properties,HrdbQueryExecutorServiceImpl.java,VacationService.java,HrdbProcedureExecutorServiceImpl.java,EmployeeController.java,UserDao.java,Vacation.java] Files modified - [index.html,Table2Controller.java,production.properties,project-services.xml,pom.xml,development.properties] Refactored files - [] Fixed bugs - [] Removed files - []

---
+++ services/hrdb/src/com/hrdb/service/HrdbProcedureExecutorService.java
@@ -0,0 +1,21 @@
+/*Copyright (c) 2015-2016 vcs2.com All Rights Reserved.
+ This software is the confidential and proprietary information of vcs2.com You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with vcs2.com*/
+
+package com.hrdb.service;
+
+import java.util.Map;
+import java.util.List;
+
+
+import com.wavemaker.runtime.data.model.CustomProcedure;
+import com.wavemaker.runtime.data.exception.QueryParameterMismatchException;
+
+public interface HrdbProcedureExecutorService {
+
+	
+	List<Object> executeWMCustomProcedure(CustomProcedure procedure) ;
+
+
+}
+
