3daf1d402688daf23e769cacddf751dd4f1d865c
root <root@13e7453603ba>
2016-09-07 07:12:43 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "VerifyVCSshare",
+  "homePage" : "Main",
+  "name" : "VerifyVCSshare",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
